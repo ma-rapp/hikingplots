4bc3fcd49091a2c4e534b4742540f9bd3e9a1817
Martin Rapp <17711336+ma-rapp@users.noreply.github.com>
2025-12-22 18:11:03 +0100

    fix np.reshape usage

--- hikingplots/plot/plot_tools.py
+++ hikingplots/plot/plot_tools.py
@@ -66,7 +66,7 @@ def plt_to_numpy(fig, dpi=1):
     io_buf.seek(0)
     img_arr = np.reshape(
         np.frombuffer(io_buf.getvalue(), dtype=np.uint8),
-        newshape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1),
+        shape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1),
     )
     io_buf.close()
 
